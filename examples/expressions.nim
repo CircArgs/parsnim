ac4dcfa945e93b295e4d4dd987e278702a89a052
nick ouellet <quebecname@gmail.com>
2022-09-06 19:15:55 -0400

    expressions working

--- examples/expressions.nim
+++ examples/expressions.nim
@@ -1,6 +1,6 @@
 import parsnim
-import std/[re, strutils]
-
+import std/[re, strutils, strformat]
+import sugar
 type
   NodeKind = enum # the different node types
     nkUnk,
@@ -17,6 +17,13 @@ type
     of nkAdd, nkSub:
       leftOp, rightOp: Node
 
+proc `$`(node: Node): string =
+  case node.kind:
+  of nkAdd, nkSub:
+    fmt"{node.kind}({node.leftOp}, {node.rightOp})"
+  else:
+    $node.kind
+
 let add = test_char('+').pad.map(nkAdd)
 let sub = test_char('-').pad.map(nkSub)
 let number = test_regex(re"[0-9]+", "number").pad.map(nkNumber)
@@ -24,7 +31,6 @@ let number = test_regex(re"[0-9]+", "number").pad.map(nkNumber)
 let e = (number or sub or add).many
 echo e
 # echo e.parse("5+ 4")
-echo e.parse("5+ 4")
 #@[nkNumber, nkAdd, nkNumber]
 
 var expression = test_item(nkUnk, "").map(Node(kind: nkUnk))
@@ -32,16 +38,17 @@ var expression = test_item(nkUnk, "").map(Node(kind: nkUnk))
 let op_expr = proc(op: NodeKind): Parser[NodeKind, Node] =
   proc expression_parser(state: var State[NodeKind]): Result[Node] =
     let index = state.stream[state.index..<state.stream.len].find(op)
+    # echo index
     if index == -1:
       return failure[Node](state.index, state.index+1, "")
     let left = expression.parse_partial(state.stream[state.index..<index])
-    echo repr(left)
     let right = expression.parse_partial(state.stream[
         index+1..<state.stream.len])
-    state.index = right.end_index
+    state.index = right.end_index + index+1
     case op:
     of nkAdd, nkSub:
-      success(state.index, right.end_index, @[Node(kind: op, leftOp: left.value[0],
+      success(left.start_index, state.index, @[Node(kind: op,
+          leftOp: left.value[0],
         rightOp: right.value[0])], "")
     else:
       raise newException(Exception, "not an operator")
@@ -50,7 +57,7 @@ let op_expr = proc(op: NodeKind): Parser[NodeKind, Node] =
 let number_expr = test_item(nkNumber, "number").map(Node(kind: nkNumber))
 expression = op_expr(nkSub) or op_expr(nkAdd) or number_expr
 
-let tokens = e.parse("5 + 4")
+let tokens = e.parse("5 - 4")
 echo tokens
 let nodes = expression.parse(tokens)
-echo repr(nodes)
+echo nodes
